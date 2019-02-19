053b1fdb4f488470b82e3b29ff51d08b1b4152ab
Jordan Irwin <antumdeluge@gmail.com>
2019-02-19 23:53:16 +0000

    Manual header rename no longer needed in generate script

--- generate.sh
+++ generate.sh
@@ -17,9 +17,3 @@ fi
 
 # generate new docs
 ldoc "${DIR_ROOT}"
-
-# modify "Modules" header to "Functions"
-# FIXME: do this within config.ld
-for F in $(find docs/ -type f -name "*.html"); do
-	sed -i -e 's|<h2>Modules</h2>|<h2>Global Functions</h2>|' "$F";
-done
